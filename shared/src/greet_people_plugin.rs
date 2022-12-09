feffcd687815605a5629dec6aeac74128d110af4
Lukas Weber <dev@sakul-flee.de>
2022-12-09 04:41:47 +0100

    Added own Timer

--- shared/src/greet_people_plugin.rs
+++ shared/src/greet_people_plugin.rs
@@ -8,9 +8,14 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
 impl Plugin for GreetPeoplePlugin {
     fn build(&self, app: &mut App) {
         app
+            // Insert our Timer as a resource
+            .insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
             // Startup Systems run once at startup
             .add_startup_system(add_persons)
             // Normal Systems run every frame
@@ -24,8 +29,12 @@ fn add_persons(mut commands: Commands) {
     commands.spawn((Person, Name("SomeFirstName SomeLastName".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for person in query.iter() {
-        println!("Hello {}!", person.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    // update our timer with the time elapsed since the last update
+    // if that caused the timer to finish, we say hello to everyone
+    if timer.0.tick(time.delta()).just_finished() {
+        for person in query.iter() {
+            println!("Hello {}!", person.0);
+        }
     }
 }
